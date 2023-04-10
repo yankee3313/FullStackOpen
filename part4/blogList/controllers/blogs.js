7e9972cdec8ec7235f0579d57fac23383b3c4c67
yankee3313 <122069684+yankee3313@users.noreply.github.com>
2023-04-09 22:58:24 -0500

    add code for exercises 4.19 and 4.20

--- part4/blogList/controllers/blogs.js
+++ part4/blogList/controllers/blogs.js
@@ -20,7 +20,7 @@ blogsRouter.post('/', async (request, response) => {
     })
   }
 
-  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
   if (!decodedToken.id) {
     return response.status(401).json({ error: 'token invalid' })
   }
