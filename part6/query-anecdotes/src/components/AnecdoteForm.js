ec24b2f8fa90f5a948ba769a07e80d8cc95bbada
yankee3313 <122069684+yankee3313@users.noreply.github.com>
2023-05-04 23:37:01 -0500

    add solutions for exercises up to 6.22

---
+++ part6/query-anecdotes/src/components/AnecdoteForm.js
@@ -0,0 +1,22 @@
+const AnecdoteForm = ({ newAnecdoteMutation }) => {
+
+  const onCreate = (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    console.log('new anecdote')
+    newAnecdoteMutation.mutate({ content, votes: 0 })
+}
+
+  return (
+    <div>
+      <h3>create new</h3>
+      <form onSubmit={onCreate}>
+        <input name='anecdote' />
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
+export default AnecdoteForm
