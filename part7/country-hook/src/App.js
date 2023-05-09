49cd6c6aeaa68f03dc4b7e4847222722aac40209
yankee3313 <122069684+yankee3313@users.noreply.github.com>
2023-05-09 14:07:19 -0500

    add solution for 7.7

---
+++ part7/country-hook/src/App.js
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
+
+const useCountry = (name) => {
+  const [country, setCountry] = useState(null)
+
+  useEffect(() => {
+    if (name) {
+      axios.get(`https://restcountries.com/v3.1/name/${name}?fullText=true`)
+        .then(response => {
+          const data = response.data[0]
+          const newData = {
+            found: true,
+            data: {
+              name: data.name.common,
+              capital: data.capital,
+              population: data.population,
+              flag: data.flags.svg}
+          }
+          setCountry(newData)
+        })
+        .catch(error => {
+          const data = {
+            found: false,
+            data: null
+          }
+          setCountry(data)
+        })
+    }
+  }, [name])
+
+  return country
+}
+
+
+const Country = ({ country }) => {
+  if (!country) {
+    return null
+  }
+
+  if (!country.found) {
+    return (
+      <div>
+        not found...
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h3>{country.data.name.common} </h3>
+      <div>capital {country.data.capital} </div>
+      <div>population {country.data.population}</div> 
+      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+    </div>
+  )
+}
+
+const App = () => {
+  const nameInput = useField('text')
+  const [name, setName] = useState('')
+  const country = useCountry(name)
+
+  const fetch = (e) => {
+    e.preventDefault()
+    setName(nameInput.value)
+  }
+
+  return (
+    <div>
+      <form onSubmit={fetch}>
+        <input {...nameInput} />
+        <button>find</button>
+      </form>
+
+      <Country country={country} />
+    </div>
+  )
+}
+
+export default App
