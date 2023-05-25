bcd5c2cb8523c0a41c362da6e0a871bd24e8103a
yankee3313 <122069684+yankee3313@users.noreply.github.com>
2023-05-25 13:35:03 -0500

    add solutions for exercises through 8.16

--- part8/library-frontend/src/index.js
+++ part8/library-frontend/src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-import { ApolloClient, ApolloProvider, InMemoryCache, gql } from '@apollo/client'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000',
