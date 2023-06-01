8d4ca6251fe9c1ad190317ec3109fa12de3bef2b
yankee3313 <122069684+yankee3313@users.noreply.github.com>
2023-06-01 17:12:02 -0500

    add solution for 9.1

---
+++ part9/bmiCalculator.ts
@@ -0,0 +1,16 @@
+const bmiCalculator = (a: number, b: number): string => {
+    let mass = a;
+    let height = b/100;
+
+    let result = (mass / (height ** 2))
+
+    if (result < 25){
+        return 'Normal (healthy weight)'
+    } else if (result >= 25 && result <= 29) {
+        return 'Overweight'
+    } else if (result >= 30) {
+        return 'Obese'
+    }   
+  }
+  
+  console.log(bmiCalculator(88.45, 198))
