f09433b16006ef854229eff555f5ad3561cc743f
yankee3313 <122069684+yankee3313@users.noreply.github.com>
2023-07-24 14:37:16 -0500

    clone patientor project try two

--- part9/patientorbackend/index.ts
+++ part9/patientorbackend/index.ts
@@ -1,13 +1,26 @@
 import express from 'express';
+const cors = require('cors');
 const app = express();
 app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:3000'
+  }));
 
 const PORT = 3001;
 
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+app.get('/api/patients', (_req, res) => {
+    // Fetch the patients data from your data source or database
+    const patients = ...; // Fetch patients from the data source
+  
+    // Send the patients data as the response
+    res.json(patients);
+  });
+  
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
