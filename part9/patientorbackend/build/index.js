de6f9d7d0da87a8540fbe83ad446ac2077a2e035
yankee3313 <122069684+yankee3313@users.noreply.github.com>
2023-07-25 15:58:36 -0500

    get frontend displaying correctly

--- part9/patientorbackend/build/index.js
+++ part9/patientorbackend/build/index.js
@@ -3,13 +3,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const app = (0, express_1.default)();
-app.use(express_1.default.json());
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
+const patients_1 = __importDefault(require("./data/patients"));
 const PORT = 3001;
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req, res) => {
     console.log('someone pinged here');
     res.send('pong');
+});
+app.get('/api/patients', (_req, res) => {
+    res.send(patients_1.default);
 });
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
--- part9/patientorbackend/index.ts
+++ part9/patientorbackend/index.ts
@@ -1,26 +1,22 @@
-import express from 'express';
+const express = require('express');
 const cors = require('cors');
 const app = express();
 app.use(express.json());
 app.use(cors({
     origin: 'http://localhost:3000'
   }));
+import patients from "./data/patients";
 
 const PORT = 3001;
 
 app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
-});
+});  
 
 app.get('/api/patients', (_req, res) => {
-    // Fetch the patients data from your data source or database
-    const patients = ...; // Fetch patients from the data source
-  
-    // Send the patients data as the response
-    res.json(patients);
-  });
-  
+  res.send(patients);
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
